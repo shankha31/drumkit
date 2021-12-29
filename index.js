80cf7086290f6fe15fdaab9d5ea7e6f6fb7d13dd
Shankha Ghosh <ghoshajoy312@gmail.com>
2021-12-29 14:45:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,31 +16,31 @@ document.addEventListener("keydown",function(e){
 function soundPlayer(key){
     switch (key) {
         case "w":
-            var wSound= new Audio("sounds/crash.mp3");
+            var wSound= new Audio("crash.mp3");
             wSound.play();
             break;
         case "a":
-            var aSound= new Audio("sounds/kick-bass.mp3");
+            var aSound= new Audio("kick-bass.mp3");
             aSound.play();
             break;    
         case "s":
-            var sSound= new Audio("sounds/snare.mp3");
+            var sSound= new Audio("snare.mp3");
             sSound.play();
             break;
         case "d":
-            var dSound= new Audio("sounds/tom-1.mp3");
+            var dSound= new Audio("tom-1.mp3");
             dSound.play();
             break;    
         case "j":
-            var jSound= new Audio("sounds/tom-2.mp3");
+            var jSound= new Audio("tom-2.mp3");
             jSound.play();
             break;    
         case "k":
-            var kSound= new Audio("sounds/tom-3.mp3");
+            var kSound= new Audio("tom-3.mp3");
             kSound.play();
             break;    
         case "l":
-            var lSound= new Audio("sounds/tom-4.mp3");
+            var lSound= new Audio("tom-4.mp3");
             lSound.play();
             break;        
         default:
